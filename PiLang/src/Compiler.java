a190e6bc4e1a8cd6bd440e010671db48720efe34
gatosyocora <kut190316q@gmail.com>
2017-11-25 13:07:05 +0900

    コンパイラ完成(演習19)

--- PiLang/src/Compiler.java
+++ PiLang/src/Compiler.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -17,22 +16,151 @@ public class Compiler extends CompilerBase {
 		for (int i = 0; i < nd.params.size(); i++) {
 			String name = nd.params.get(i);
 			int offset = 4 * (i + 1);
+
 			LocalVariable var = new LocalVariable(name, offset);
 			env.push(var);
 		}
-
-		/* ここにプログラムを追加する */
+		
+		emitLabel(nd.name);
+		System.out.println("\t@prologue");
+		emitPUSH(REG_FP);
+		emitRR("mov", REG_FP, REG_SP);
+		emitPUSH(REG_LR);
+		emitPUSH(REG_R1);
+		emitRRI("sub", REG_SP, REG_SP, nd.varDecls.size() * 4);
+		
+		for (int i = 0; i < nd.varDecls.size(); i++) {
+			String name = nd.varDecls.get(i);
+			int offset = -4 * (i + 3);
+			LocalVariable var = new LocalVariable(name, offset);
+			env.push(var);
+		}
+		for (ASTNode stmt: nd.stmts)
+			compileStmt(stmt, epilogueLabel, env);
+		emitRI("mov", REG_DST, 0); // returnがなかったときの戻り値0
+		emitLabel(epilogueLabel);
+		System.out.println("\t@epilogue");
+		emitRRI("add", REG_SP, REG_SP, nd.varDecls.size() * 4);
+		emitPOP(REG_R1);
+		emitPOP(REG_LR);
+		emitPOP(REG_FP);
+		emitRET();
 	}
 	
 	void compileStmt(ASTNode ndx, String epilogueLabel, Environment env) {
-		/* ここを完成させる */
+		
+		if (ndx instanceof ASTCompoundStmtNode) {
+			ASTCompoundStmtNode nd = (ASTCompoundStmtNode) ndx;
+			for (ASTNode stmt: nd.stmts) {
+				compileStmt(stmt, epilogueLabel, env);
+				emitJMP("b", epilogueLabel);
+			}
+		} else if (ndx instanceof ASTAssignStmtNode) {
+			ASTAssignStmtNode nd = (ASTAssignStmtNode) ndx;
+			Variable var = env.lookup(nd.var);
+			if (var == null)
+				var = globalEnv.lookup(nd.var);
+			if (var == null) 
+				throw new Error("undefined variable: " + nd.var);
+			compileExpr(nd.expr, env);
+			if (var instanceof GlobalVariable) {
+				GlobalVariable globalVar = (GlobalVariable) var;
+				emitLDC(REG_R1, globalVar.getLabel());
+				emitSTR(REG_DST, REG_R1, 0);
+			} else {
+				LocalVariable localVar = (LocalVariable) var;
+				int offset = localVar.offset;
+				emitSTR(REG_DST, REG_FP, offset);
+			}
+		} else if (ndx instanceof ASTIfStmtNode) {
+			ASTIfStmtNode nd = (ASTIfStmtNode) ndx;
+			String elseLabel = freshLabel();
+			String endLabel = freshLabel();
+			compileExpr(nd.cond, env);
+			emitRI("cmp", REG_DST, 0);
+			emitJMP("beq", elseLabel);
+			compileStmt(nd.thenClause, epilogueLabel, env);
+			emitJMP("b", epilogueLabel);
+			emitJMP("b", endLabel);
+			emitLabel(elseLabel);
+			compileStmt(nd.elseClause, epilogueLabel, env);
+			emitJMP("b", epilogueLabel);
+			emitLabel(endLabel);
+		} else if (ndx instanceof ASTWhileStmtNode) {
+			ASTWhileStmtNode nd = (ASTWhileStmtNode) ndx;
+			String startLabel = freshLabel();
+			String endLabel = freshLabel();
+			emitLabel(startLabel);
+			compileExpr(nd.cond, env);
+			emitRI("cmp", REG_DST, 1);
+			emitJMP("beq", endLabel);
+			compileStmt(nd.stmt, epilogueLabel, env);
+			emitJMP("b", epilogueLabel);
+			emitJMP("b", startLabel);
+			emitLabel(endLabel);
+		} else if (ndx instanceof ASTReturnNode) {
+			ASTReturnNode nd = (ASTReturnNode) ndx;
+			compileExpr(nd.expr, env);
+		} else
+			throw new Error("Unknown expression: " + ndx);
 	}
 
 	void compileExpr(ASTNode ndx, Environment env) {
-		/* ここを完成させる */
+		if (ndx instanceof ASTBinaryExprNode) {
+			ASTBinaryExprNode nd = (ASTBinaryExprNode) ndx;
+			compileExpr(nd.lhs, env);
+			emitPUSH(REG_R1);
+			emitRR("mov", REG_R1, REG_DST);
+			compileExpr(nd.rhs, env);
+			if (nd.op.equals("+"))
+				emitRRR("add", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("-"))
+				emitRRR("sub", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("*"))
+				emitRRR("mul", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("/"))
+				emitRRR("udiv", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("&")) 
+				emitRRR("and", REG_DST, REG_R1, REG_DST);
+			else if (nd.op.equals("|")) 
+				emitRRR("orr", REG_DST, REG_R1, REG_DST);
+			else
+				throw new Error("Unknwon operator: "+nd.op);
+			emitPOP(REG_R1);
+		} else if (ndx instanceof ASTCallNode) {
+			ASTCallNode nd = (ASTCallNode) ndx;
+			for (int i = nd.args.size()-1;i >= 0;i--) {
+				ASTNode arg = nd.args.get(i);
+				compileExpr(arg, env);
+				emitPUSH(REG_DST);
+			}
+			emitCALL(nd.name);
+			emitRRI("add", REG_SP, REG_SP, nd.args.size() * 4);
+		} else if (ndx instanceof ASTNumberNode) {
+			ASTNumberNode nd = (ASTNumberNode) ndx;
+			emitLDC(REG_DST, nd.value);
+		} else if (ndx instanceof ASTVarRefNode) {
+			ASTVarRefNode nd = (ASTVarRefNode) ndx;
+			Variable var = env.lookup(nd.varName);
+			if (var == null)
+				var = globalEnv.lookup(nd.varName);
+			if (var == null)
+				throw new Error("Undefined variable: " + nd.varName);
+			if (var instanceof GlobalVariable) {
+				GlobalVariable globalVar = (GlobalVariable) var;
+				emitLDC(REG_DST, globalVar.getLabel());
+				emitLDR(REG_DST, REG_DST, 0);
+			} else {
+				LocalVariable localVar = (LocalVariable) var;
+				int offset = localVar.offset;
+				emitLDR(REG_DST, REG_FP, offset);
+			}
+		} else
+			throw new Error("Unknown expression: "+ndx);
 	}
 	
 	void compile(ASTNode ast) {
+
 		globalEnv = new Environment();
 		ASTProgNode program = (ASTProgNode) ast;
 
